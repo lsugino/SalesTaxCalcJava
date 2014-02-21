3e2e133cc606fdf630e9aa3705f3f71d8063ba3b
Lindsey Sugino <lindseysugino@gmail.com>
2014-02-20 19:46:56 -0800

    reformat file parser

--- src/FileParser.java
+++ src/FileParser.java
@@ -3,63 +3,86 @@ import java.util.ArrayList;
 
 class FileParser {
 	
-	public File receiptToParse;
-	
-	public void /*ArrayList<String>*/ fileParser (File receipt) {
+	private File receiptToParse;
+    private FileParser newFile;
+
+	public void FileParser (File receipt) {
 		receiptToParse = receipt;
 		FileParser newFile = new FileParser();
-		newFile.parseFile(receiptToParse);
+		newFile.parserController(receiptToParse);
+        System.out.println("test");
 	}
-	
-//	public void parserController (String args[]) {
-//		FileParser newFile = new FileParser();
-//		newFile.parseFile(receiptToParse);
-//		System.out.println("does this hit?");
-//	}
-	
-	public void /*ArrayList<String>*/ parseFile(File receiptToParse) {
-      try {
-    	  BufferedReader reader = new BufferedReader(new FileReader(receiptToParse));
-    	  String strLine;
-    	  
-    	  while ((strLine = reader.readLine()) != null) {
-    		System.out.println("Test input from file:");
-    		System.out.println(strLine);
-    		
-            // Split each word and put it into a string array 
-    		String[] info = strLine.split(" ");
-    		
-    		// arrayList to hold updated array
-    		ArrayList<String> updArray = new ArrayList<String>();
-    		updArray.add(info[0]);
-    		
-    		// build a string for the description + at    		
-    		StringBuilder builder = new StringBuilder(info.length);
 
-    		// grab the description and put into a string
-            for (int i = 1; i < info.length - 1; i++)
-                builder.append(info[i] + " ");
-            
-    		// grab the description and add to updated array
-            updArray.add(builder.toString());
-
-    		// add the last item in the original array and add to updated array
-            updArray.add(info[info.length -1]);
-            
-    		// test value of updated array
-	    	System.out.println("Each value in ArrayList:");
-	        System.out.println(updArray.get(0));
-	        System.out.println(updArray.get(1));
-	        System.out.println(updArray.get(2));
-            
-//      	return updArray;
+    public void parserController(File receiptToParse) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(receiptToParse));
+            String strLine;
+            ArrayList<String> updArray;
           
-    	  }
-    	  reader.close();
+            while ((strLine = reader.readLine()) != null) {
+                System.out.println("Test input from file:");
+                System.out.println(strLine);
 
-       	} catch (Exception e) {
-        	System.err.println("Error: " + e.getMessage());
+                String[] splitArray = toStrArray(strLine);
+                System.out.println(splitArray[0]);
+
+                updArray = addQty(splitArray);
+                System.out.println(updArray.get(0));
+
+                String desc = formatDesc(splitArray);
+                System.out.println(desc);
+
+                // ArrayList<String> newArr = addDesc(desc);
+                // System.out.println(newArr.get(0));
+                // System.out.println(newArr.get(1));
+
+                // newFile.addDesc(splitArray);
+                // newFile.addPrice(splitArray);
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
         }
-   
-    } // end of
+	}
+
+    // Split each word and put it into a string array 
+	public String[] toStrArray(String strLine) {
+		String[] splitArray = strLine.split(" ");
+        System.out.println("testing");   
+        return splitArray;
+    }
+
+    // arrayList to hold updated array
+    public ArrayList<String> addQty(String[] splitArray) {
+        ArrayList<String> updArray = new ArrayList<String>();
+        updArray.add(splitArray[0]);
+        return updArray;
+    }
+
+    // build a string for the description      
+    public String formatDesc(String[] splitArray) {
+        StringBuilder builder = new StringBuilder(splitArray.length);
+
+        // grab the description and put into a string
+        for (int i = 1; i < splitArray.length - 1; i++){
+             builder.append(splitArray[i] + " ");
+        }
+        String desc = builder.toString();
+        System.out.println(desc);
+        return desc;
+    }
+
+    // grab the description and add to updated array
+    // DOESN'T WORK
+    // public ArrayList<String> addDesc(String desc) {
+    //     updArray.add(desc);
+    //     return updArray;
+    // }
+                
+    // add the last item in the original array and add to updated array
+    // public void addPrice(String desc) {
+        // updArray.add(splitArray[splitArray.length -1]);
+    // }
+            
 }
+    		
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String args[]) {
 		File receipt = new File("/Users/lsugino/Dropbox/Programming/SalesTax-Calculator/Input1.txt");
 		FileParser receiptInput = new FileParser();
-		receiptInput.fileParser(receipt);
+		receiptInput.FileParser(receipt);
 	}
 }
